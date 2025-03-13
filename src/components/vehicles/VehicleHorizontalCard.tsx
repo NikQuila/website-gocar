7a8b242dc9f21636ab1dc6de27f08d42b3f7645a
Clemente Silva <clementesilvavicuna@gmail.com>
2025-03-12 22:14:32 -0300

    fixingAutosVendidos

--- src/components/vehicles/VehicleHorizontalCard.tsx
+++ src/components/vehicles/VehicleHorizontalCard.tsx
@@ -1,4 +1,4 @@
-'use client';
+"use client";
 import {
   Card,
   CardBody,
@@ -6,13 +6,13 @@ import {
   Image,
   Chip,
   Divider,
-} from '@heroui/react';
-import { Vehicle } from '../../utils/types';
-import { useRouter } from 'next/navigation';
+} from "@heroui/react";
+import { Vehicle } from "../../utils/types";
+import { useRouter } from "next/navigation";
 import {
   mapFuelTypeToSpanish,
   mapTransmissionTypeToSpanish,
-} from '@/utils/functions';
+} from "@/utils/functions";
 
 interface VehicleHorizontalCardProps {
   vehicle: Vehicle;
@@ -21,9 +21,9 @@ interface VehicleHorizontalCardProps {
 const VehicleHorizontalCard = ({ vehicle }: VehicleHorizontalCardProps) => {
   const router = useRouter();
 
-  const formattedPrice = new Intl.NumberFormat('es-CL', {
-    style: 'currency',
-    currency: 'CLP',
+  const formattedPrice = new Intl.NumberFormat("es-CL", {
+    style: "currency",
+    currency: "CLP",
   }).format(vehicle.price);
 
   const discountedPrice = vehicle.discount_percentage
@@ -34,12 +34,12 @@ const VehicleHorizontalCard = ({ vehicle }: VehicleHorizontalCardProps) => {
     ? vehicle.price - (discountedPrice || 0)
     : 0;
 
-  const formattedSavings = new Intl.NumberFormat('es-CL', {
-    style: 'currency',
-    currency: 'CLP',
+  const formattedSavings = new Intl.NumberFormat("es-CL", {
+    style: "currency",
+    currency: "CLP",
   }).format(savingsAmount);
 
-  const isSold = vehicle.status === 'sold';
+  const isSold = vehicle.status === "sold";
 
   const handleViewDetails = () => {
     router.push(`/vehicles/${vehicle.id}`);
@@ -47,23 +47,33 @@ const VehicleHorizontalCard = ({ vehicle }: VehicleHorizontalCardProps) => {
 
   return (
     <Card
-      isPressable
+      isPressable={!isSold}
       onPress={handleViewDetails}
-      className='overflow-hidden flex flex-row bg-white dark:bg-dark-card'
+      className={`overflow-hidden flex flex-row bg-white dark:bg-dark-card ${
+        isSold ? "opacity-75" : ""
+      }`}
     >
-      <div className='w-[300px] flex-shrink-0'>
-        <div className='relative w-full h-full'>
+      <div className="w-[300px] flex-shrink-0">
+        <div className="relative w-full h-full">
+          {isSold && (
+            <div className="absolute top-0 right-0 h-[200px] w-[200px] overflow-hidden z-50 rotate-0">
+              <div className="absolute top-[30px] right-[-50px] bg-sold text-white font-bold py-2 w-[250px] text-center transform rotate-45">
+                VENDIDO
+              </div>
+            </div>
+          )}
           <Image
             alt={`${vehicle.brand?.name} ${vehicle.model?.name}`}
-            className='object-cover h-full w-full'
+            className="object-cover h-full w-full"
             src={vehicle.main_image}
           />
-          {vehicle.discount_percentage !== undefined &&
+          {!isSold &&
+            vehicle.discount_percentage !== undefined &&
             vehicle.discount_percentage > 0 && (
-              <div className='absolute bottom-2 right-2 z-10'>
+              <div className="absolute bottom-2 right-2 z-10">
                 <Chip
-                  size='sm'
-                  className='shadow-lg text-white bg-gradient-to-r from-orange-500 to-red-500 border-none'
+                  size="sm"
+                  className="shadow-lg text-white bg-gradient-to-r from-orange-500 to-red-500 border-none"
                 >
                   Ahorra {formattedSavings}
                 </Chip>
@@ -72,43 +82,43 @@ const VehicleHorizontalCard = ({ vehicle }: VehicleHorizontalCardProps) => {
         </div>
       </div>
 
-      <div className='flex flex-col flex-1'>
-        <CardBody className='flex flex-col flex-grow p-4 relative'>
-          <div className='space-y-2'>
-            <div className='flex items-start justify-between'>
+      <div className="flex flex-col flex-1">
+        <CardBody className="flex flex-col flex-grow p-4 relative">
+          <div className="space-y-2">
+            <div className="flex items-start justify-between">
               <div>
-                <p className='text-sm text-gray-600 dark:text-gray-400'>
+                <p className="text-sm text-gray-600 dark:text-gray-400">
                   {vehicle.brand?.name} {vehicle.year}
                 </p>
-                <h3 className='text-xl font-bold dark:text-dark-text mt-1'>
+                <h3 className="text-xl font-bold dark:text-dark-text mt-1">
                   {vehicle.model?.name}
                 </h3>
               </div>
-              <div className='text-right'>
+              <div className="text-right">
                 {vehicle.discount_percentage ? (
                   <>
-                    <p className='text-base line-through text-gray-400 dark:text-gray-500'>
+                    <p className="text-base line-through text-gray-400 dark:text-gray-500">
                       {formattedPrice}
                     </p>
-                    <p className='text-2xl font-bold bg-gradient-to-r from-orange-500 to-red-500 bg-clip-text text-transparent'>
-                      {new Intl.NumberFormat('es-CL', {
-                        style: 'currency',
-                        currency: 'CLP',
+                    <p className="text-2xl font-bold bg-gradient-to-r from-orange-500 to-red-500 bg-clip-text text-transparent">
+                      {new Intl.NumberFormat("es-CL", {
+                        style: "currency",
+                        currency: "CLP",
                       }).format(discountedPrice!)}
                     </p>
                   </>
                 ) : (
-                  <p className='text-2xl font-bold text-primary dark:text-white'>
+                  <p className="text-2xl font-bold text-primary dark:text-white">
                     {formattedPrice}
                   </p>
                 )}
               </div>
             </div>
 
-            <div className='flex items-center gap-2 text-sm text-gray-600 dark:text-gray-400'>
+            <div className="flex items-center gap-2 text-sm text-gray-600 dark:text-gray-400">
               <span>{vehicle.mileage.toLocaleString()} km</span>
               <span>•</span>
-              <span className='capitalize'>{vehicle?.fuel_type_new?.name}</span>
+              <span className="capitalize">{vehicle?.fuel_type_new?.name}</span>
               <span>•</span>
               <span>{mapTransmissionTypeToSpanish(vehicle.transmission)}</span>
             </div>
@@ -117,13 +127,13 @@ const VehicleHorizontalCard = ({ vehicle }: VehicleHorizontalCardProps) => {
               {vehicle.description}
             </p> */}
 
-            <div className='flex flex-wrap gap-2 mt-2'>
+            <div className="flex flex-wrap gap-2 mt-2">
               {vehicle.features.slice(0, 5).map((feature, index) => (
                 <Chip
                   key={index}
-                  size='sm'
-                  variant='flat'
-                  className='bg-gray-100 dark:bg-dark-border dark:text-dark-text'
+                  size="sm"
+                  variant="flat"
+                  className="bg-gray-100 dark:bg-dark-border dark:text-dark-text"
                 >
                   {feature}
                 </Chip>
--- src/components/vehicles/VehicleVerticalCard.tsx
+++ src/components/vehicles/VehicleVerticalCard.tsx
@@ -1,4 +1,4 @@
-'use client';
+"use client";
 import {
   Card,
   CardBody,
@@ -6,13 +6,13 @@ import {
   Image,
   Chip,
   Divider,
-} from '@heroui/react';
-import { Vehicle } from '../../utils/types';
-import { useRouter } from 'next/navigation';
+} from "@heroui/react";
+import { Vehicle } from "../../utils/types";
+import { useRouter } from "next/navigation";
 import {
   mapFuelTypeToSpanish,
   mapTransmissionTypeToSpanish,
-} from '@/utils/functions';
+} from "@/utils/functions";
 
 interface VehicleVerticalCardProps {
   vehicle: Vehicle;
@@ -21,9 +21,9 @@ interface VehicleVerticalCardProps {
 const VehicleVerticalCard = ({ vehicle }: VehicleVerticalCardProps) => {
   const router = useRouter();
 
-  const formattedPrice = new Intl.NumberFormat('es-CL', {
-    style: 'currency',
-    currency: 'CLP',
+  const formattedPrice = new Intl.NumberFormat("es-CL", {
+    style: "currency",
+    currency: "CLP",
   }).format(vehicle.price);
 
   const discountedPrice = vehicle.discount_percentage
@@ -34,12 +34,12 @@ const VehicleVerticalCard = ({ vehicle }: VehicleVerticalCardProps) => {
     ? vehicle.price - (discountedPrice || 0)
     : 0;
 
-  const formattedSavings = new Intl.NumberFormat('es-CL', {
-    style: 'currency',
-    currency: 'CLP',
+  const formattedSavings = new Intl.NumberFormat("es-CL", {
+    style: "currency",
+    currency: "CLP",
   }).format(savingsAmount);
 
-  const isSold = vehicle.status === 'sold';
+  const isSold = vehicle.status === "sold";
 
   const handleViewDetails = () => {
     router.push(`/vehicles/${vehicle.id}`);
@@ -47,23 +47,33 @@ const VehicleVerticalCard = ({ vehicle }: VehicleVerticalCardProps) => {
 
   return (
     <Card
-      isPressable
+      isPressable={!isSold}
       onPress={handleViewDetails}
-      className='overflow-hidden bg-white dark:bg-dark-card'
+      className={`overflow-hidden bg-white dark:bg-dark-card ${
+        isSold ? "opacity-75" : ""
+      }`}
     >
-      <div className='w-full'>
-        <div className='relative w-full aspect-[16/9]'>
+      <div className="w-full">
+        <div className="relative w-full aspect-[16/9]">
+          {isSold && (
+            <div className="absolute top-0 right-0 h-[200px] w-[200px] overflow-hidden z-50 rotate-0">
+              <div className="absolute top-[30px] right-[-50px] bg-sold text-white font-bold py-2 w-[250px] text-center transform rotate-45">
+                VENDIDO
+              </div>
+            </div>
+          )}
           <Image
             alt={`${vehicle.brand?.name} ${vehicle.model?.name}`}
-            className='object-cover w-full h-full'
+            className="object-cover w-full h-full"
             src={vehicle.main_image}
           />
-          {vehicle.discount_percentage !== undefined &&
+          {!isSold &&
+            vehicle.discount_percentage !== undefined &&
             vehicle.discount_percentage > 0 && (
-              <div className='absolute bottom-2 right-2 z-10'>
+              <div className="absolute bottom-2 right-2 z-10">
                 <Chip
-                  size='sm'
-                  className='shadow-lg text-white bg-gradient-to-r from-orange-500 to-red-500 border-none'
+                  size="sm"
+                  className="shadow-lg text-white bg-gradient-to-r from-orange-500 to-red-500 border-none"
                 >
                   Ahorra {formattedSavings}
                 </Chip>
@@ -72,34 +82,34 @@ const VehicleVerticalCard = ({ vehicle }: VehicleVerticalCardProps) => {
         </div>
       </div>
 
-      <div className='flex flex-col'>
-        <CardBody className='flex flex-col flex-grow p-4 '>
-          <div className='space-y-1.5'>
-            <div className='flex items-center justify-between'>
-              <p className='text-sm text-gray-600 dark:text-gray-400'>
+      <div className="flex flex-col">
+        <CardBody className="flex flex-col flex-grow p-4 ">
+          <div className="space-y-1.5">
+            <div className="flex items-center justify-between">
+              <p className="text-sm text-gray-600 dark:text-gray-400">
                 {vehicle.brand?.name} {vehicle.year}
               </p>
             </div>
 
-            <h3 className='text-xl font-bold dark:text-dark-text'>
+            <h3 className="text-xl font-bold dark:text-dark-text">
               {vehicle.model?.name}
             </h3>
 
-            <div className='flex items-center gap-2 text-sm text-gray-600 dark:text-gray-400'>
+            <div className="flex items-center gap-2 text-sm text-gray-600 dark:text-gray-400">
               <span>{vehicle.mileage.toLocaleString()} km</span>
               <span>•</span>
-              <span className='capitalize'>{vehicle?.fuel_type_new?.name}</span>
+              <span className="capitalize">{vehicle?.fuel_type_new?.name}</span>
               <span>•</span>
               <span>{mapTransmissionTypeToSpanish(vehicle.transmission)}</span>
             </div>
 
-            <div className='flex flex-wrap gap-2'>
+            <div className="flex flex-wrap gap-2">
               {vehicle.features.slice(0, 3).map((feature, index) => (
                 <Chip
                   key={index}
-                  size='sm'
-                  variant='flat'
-                  className='bg-gray-100 dark:bg-dark-border dark:text-dark-text'
+                  size="sm"
+                  variant="flat"
+                  className="bg-gray-100 dark:bg-dark-border dark:text-dark-text"
                 >
                   {feature}
                 </Chip>
@@ -108,24 +118,24 @@ const VehicleVerticalCard = ({ vehicle }: VehicleVerticalCardProps) => {
           </div>
         </CardBody>
 
-        <Divider className='dark:border-dark-border' />
+        <Divider className="dark:border-dark-border" />
 
-        <CardFooter className='p-4'>
-          <div className='w-full text-left'>
+        <CardFooter className="p-4">
+          <div className="w-full text-left">
             {vehicle.discount_percentage ? (
               <>
-                <p className='text-sm line-through text-gray-400 dark:text-gray-500 text-left'>
+                <p className="text-sm line-through text-gray-400 dark:text-gray-500 text-left">
                   {formattedPrice}
                 </p>
-                <p className='text-xl font-semibold bg-gradient-to-r from-orange-500 to-red-500 bg-clip-text text-transparent text-left'>
-                  {new Intl.NumberFormat('es-CL', {
-                    style: 'currency',
-                    currency: 'CLP',
+                <p className="text-xl font-semibold bg-gradient-to-r from-orange-500 to-red-500 bg-clip-text text-transparent text-left">
+                  {new Intl.NumberFormat("es-CL", {
+                    style: "currency",
+                    currency: "CLP",
                   }).format(discountedPrice!)}
                 </p>
               </>
             ) : (
-              <p className='text-xl font-semibold text-primary dark:text-white text-left'>
+              <p className="text-xl font-semibold text-primary dark:text-white text-left">
                 {formattedPrice}
               </p>
             )}
