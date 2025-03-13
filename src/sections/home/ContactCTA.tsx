a35c7cb0417c16fb9b1c4c174ad4ae95c0a9b306
NikQuila <nicopirozzi1@gmail.com>
2025-03-13 17:03:06 -0300

    contact

--- src/sections/home/ContactCTA.tsx
+++ src/sections/home/ContactCTA.tsx
@@ -4,13 +4,15 @@ import Link from 'next/link';
 
 const ContactCTA = () => {
   return (
-    <section className='bg-primary'>
+    <section className='bg-white dark:bg-dark-card'>
       <div className='max-w-7xl mx-auto py-12 px-4 sm:px-6 lg:py-16 lg:px-8 lg:flex lg:items-center lg:justify-between'>
         <h2 className='text-3xl font-extrabold tracking-tight text-secondary sm:text-4xl'>
-          <span className='block'>
+          <span className='block text-black dark:text-white'>
             ¿Listo para encontrar tu próximo vehículo?
           </span>
-          <span className='block'>Contáctanos hoy mismo.</span>
+          <span className='block text-black dark:text-white'>
+            Contáctanos hoy mismo.
+          </span>
         </h2>
         <div className='mt-8 flex lg:mt-0 lg:flex-shrink-0'>
           <Link href='/contact' prefetch>
@@ -18,7 +20,7 @@ const ContactCTA = () => {
               color='primary'
               variant='shadow'
               size='lg'
-              className='font-semibold hover:scale-105 bg-secondary hover:bg-secondary/90 text-primary transition-all duration-300'
+              className='font-semibold hover:scale-105 bg-primary hover:bg-primary/90 text-secondary transition-all duration-300'
             >
               Contactar
             </Button>
