4744ab40374f873837fe3d73e8b18866beaceb0f
clemesilva <161178798+clemesilva@users.noreply.github.com>
2025-04-25 09:39:37 -0400

    arregloTitulo

--- src/app/purchase-consignment/page.tsx
+++ src/app/purchase-consignment/page.tsx
@@ -289,7 +289,9 @@ ${formData.message}
   return (
     <div className='max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-16  dark:bg-dark-bg transition-colors duration-300 mt-10 text-center mb-10'>
       <h1 className='text-4xl font-bold text-gray-900 dark:text-white sm:text-5xl uppercase tracking-wide'>
-        FORMULARIO COMPRA / CONSIGNACIÓN
+        FORMULARIO
+        <br />
+        COMPRA / CONSIGNACIÓN
       </h1>
       <p className='mt-4 text-xl text-gray-600 dark:text-gray-400 mb-8'>
         Completa los datos para vender o consignar tu vehículo con nosotros
